58e64edaac7d219c56b39dff754d91e049070160
Ava <atikekar@umich.edu>
2025-07-27 14:32:17 -0400

    hello

--- parser.py
+++ parser.py
@@ -102,7 +102,7 @@ def find_total_energy(page_lines):
     return 100  # Return the list of energy values
 
 # Function to generate page data and CSV
-def find_page_data(page, page_num):
+def find_page_data(page, data, page_num):
     page_data = []
 
     month_in = find_month(page)
@@ -111,7 +111,7 @@ def find_page_data(page, page_num):
     st.write(year_in)
     name_in = find_name(page)  # Pass the file_bytes to find_name
     st.write(name_in)
-    total_in = find_total_energy(page)
+    total_in = find_total_energy(data)
     st.write(total_in)
     page_data.append(Page(page_num, month_in, year_in, name_in, total_in))
 
@@ -161,13 +161,14 @@ def execute():
         st.write(f"Total pages in PDF: {len(pdf.pages)}")
 
         for i, page in enumerate(pdf.pages):
-            data = page.extract_tables()
-            lines = data.splitlines() if data else []
+            text = page.extract_text()
+            table = page.extract_tables()
+            lines = text.splitlines() if text else []
             page_num = i + 1
 
             progress_bar.progress(min(10 + ((i + 1) * 10), 90), f"Processing page {i + 1} of {len(pdf.pages)}")
             st.write("Extracting data from page", page_num)
-            page_data.append(find_page_data(lines, page_num))
+            page_data.append(find_page_data(lines, table, page_num))
 
     input_file_name = input_file.name if input_file.name else "extracted_data.pdf"
     csv_name = input_file_name.replace('.pdf', '_data.csv')
