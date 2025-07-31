a8f4ddb275cda64ed09d9b57faf8d0b336aca1cb
Ava <atikekar@umich.edu>
2025-07-31 10:42:16 -0400

    Hello

--- parser.py
+++ parser.py
@@ -81,8 +81,7 @@ def find_total_energy(page_lines):
     table_values = []
     
     for line in page_lines:
-        st.write("Line: ", line)
-        match = re.match(r'^\d+.*\s+(\d+)$', line.strip())
+        match = re.match(r'^\d', line.strip())
         if match:
             table_values.append(line)
     st.write(table_values)
