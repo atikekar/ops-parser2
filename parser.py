112d2502d5acdc819d4d7feafddd3a339235f4b6
Ava <atikekar@umich.edu>
2025-07-30 20:37:44 -0400

    Hello

--- parser.py
+++ parser.py
@@ -80,7 +80,7 @@ def find_total_energy(page_lines):
     extracted_table = []
     for row in page_lines: 
         #if row starts with 01-31, 1-31 or a date format
-        if re.match(r'^\d{1,2}[-/]\d{1,2}[-/]\d{4}', row):
+        if re.match(r'^\s*\d', row):
             extracted_table.append(row)
     
     st.write("Extracted Table:", extracted_table)
