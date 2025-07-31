c2b1b7c08a4a5f09ba4818a7ac7db8e4f575363d
Ava <atikekar@umich.edu>
2025-07-31 10:32:16 -0400

    Hello

--- parser.py
+++ parser.py
@@ -150,7 +150,7 @@ def execute():
 
             progress_bar.progress(min(10 + ((i + 1) * 10), 90), f"Processing page {i + 1} of {len(pdf.pages)}")
             st.write("Extracting data from page", page_num)
-            page_data.append(find_page_data(lines, page_num))
+            page_data.extend(find_page_data(lines, page_num))
 
     input_file_name = input_file.name if input_file.name else "extracted_data.pdf"
     csv_name = input_file_name.replace('.pdf', '_data.csv')
