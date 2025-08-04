96a4a6565ce89bc1d5b152081043f8e9d5273eee
Ava <atikekar@umich.edu>
2025-08-04 18:09:23 -0400

    Smart Extraction

--- parser.py
+++ parser.py
@@ -125,6 +125,8 @@ def find_total_energy(page_lines, extract_mode):
             st.write(table[-1])
             header = head[-1].replace(" ", "X")
             values = table[-1].replace(" ", "X")
+            st.write(header)
+            st.write(values)
 
 
             keys = ["Energy", "Usage", "MMBtu", "Rounded"]
