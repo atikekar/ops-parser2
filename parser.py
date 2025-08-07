0ea6d5c3df3f94152d9e1df2f2212bf3436c05e9
Ava <atikekar@umich.edu>
2025-08-07 11:25:30 -0400

    README

--- parser.py
+++ parser.py
@@ -120,8 +120,7 @@ def find_total_energy(page_lines, extract_mode, page_num):
 
         elif extract_mode == "Manual Extract":
             stripped_lines = [line for line in page_lines if line.strip() != '']
-            #st.write(stripped_lines)
-            st.text_area("Page Preview", value=stripped_lines, height=300)
+            st.write(stripped_lines)
             energy_value = st.number_input(f"Enter Total Energy For Page {page_num}: ", min_value=0.0, key=f"energy_input_{page_num}")
             return energy_value
     except Exception as e:
